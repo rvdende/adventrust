4e860e7baa3d2b43da9471e679e365912ddd9650
Rouan van der Ende <rouan@8bo.org>
2023-03-21 12:59:46 +0200

    2021 day-06 part2

--- 2021/day-06/src/main.rs
+++ 2021/day-06/src/main.rs
@@ -1,11 +1,19 @@
 struct Survey {
     fish: Vec<Fish>,
+    fishgroups: Vec<FishGroup>,
     day: isize,
 }
 
 #[derive(Debug)]
 struct Fish {
     timer: isize,
+}
+
+#[derive(Debug)]
+struct FishGroup {
+    /** days left till spawn */
+    timer: isize,
+    count: i128,
 }
 
 impl Survey {
@@ -19,17 +27,46 @@ impl Survey {
             })
             .collect::<Vec<Fish>>();
 
-        Self { fish, day: 0 }
+        let mut fishgroups: Vec<FishGroup> = Vec::new();
+
+        for timer in 0..9 {
+            let count: i128 = fish
+                .iter()
+                .filter(|f| f.timer == timer)
+                .count()
+                .try_into()
+                .unwrap();
+
+            fishgroups.push(FishGroup {
+                timer: timer,
+                count,
+            })
+        }
+
+        Self {
+            fish,
+            day: 0,
+            fishgroups,
+        }
     }
 
     fn print(&self) {
         print!("After \t{} days: ", self.day);
-        for f in 0..self.fish.len() {
-            if f != 0 {
-                print!(",");
-            }
-            print!("{}", self.fish[f].timer);
+        // for f in 0..self.fish.len() {
+        //     if f != 0 {
+        //         print!(",");
+        //     }
+        //     print!("{}", self.fish[f].timer);
+        // }
+        println!();
+
+        for g in 0..self.fishgroups.len() {
+            println!(
+                "Group {} Timer: {} Fishes: {}",
+                g, self.fishgroups[g].timer, self.fishgroups[g].count
+            );
         }
+
         println!();
     }
 
@@ -53,6 +90,33 @@ impl Survey {
 
         // self.fish.retain(|fish| fish.timer > 0);
     }
+
+    fn advance_day_group(&mut self) {
+        self.day += 1;
+
+        let mut fishToReset = 0;
+        let mut fishToSpawn = 0;
+
+        for g in 0..self.fishgroups.len() {
+            if g == 0 {
+                fishToReset = self.fishgroups[0].count;
+                fishToSpawn = self.fishgroups[0].count;
+                self.fishgroups[0].count = 0; // clears the bottom group
+            }
+
+            if g < self.fishgroups.len() - 1 {
+                self.fishgroups[g].count = self.fishgroups[g + 1].count; // move value down from above.
+            }
+        }
+
+        self.fishgroups[6].count += fishToReset;
+        self.fishgroups[8].count = fishToSpawn;
+    }
+
+    fn getTotalFish(&self) -> i128 {
+        let total: i128 = self.fishgroups.iter().map(|g| g.count).sum();
+        return total;
+    }
 }
 
 fn main() {
@@ -61,15 +125,15 @@ fn main() {
 
     survey.print();
 
-    let totaldays = 80;
+    let totaldays = 256;
 
     loop {
-        survey.advance_day();
+        survey.advance_day_group();
         survey.print();
         if survey.day >= totaldays {
             break;
         }
     }
 
-    println!("Total fish: {}", survey.fish.len());
+    println!("Total fish: {}", survey.getTotalFish());
 }
