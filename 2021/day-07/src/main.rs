e9f41cd3edbdba4593bd6d913e5f412ded11ff5c
Rouan van der Ende <rouan@8bo.org>
2023-03-21 13:42:13 +0200

    2021 day-07 part 2

--- 2021/day-07/src/main.rs
+++ 2021/day-07/src/main.rs
@@ -1,5 +1,8 @@
-fn calcfuel(data: &Vec<isize>, pos: isize) -> isize {
-    let output: isize = data.iter().map(|c| (c - pos).abs()).sum();
+fn calcfuel(data: &Vec<isize>, pos: isize) -> usize {
+    let output: usize = data
+        .iter()
+        .map(|c| calcexpfuel((c - pos).abs() as usize))
+        .sum();
     return output;
 }
 
@@ -19,8 +22,8 @@ fn main() {
         .map(|f| f.parse::<isize>().unwrap())
         .collect::<Vec<_>>();
 
-    let fuel = calcfuel(&data, 2);
-    println!("fuel {}", fuel);
+    // let fuel: isize = calcfuel(&data, 2) as isize;
+    // println!("fuel {}", fuel);
 
     let minmax = getminmax(&data);
     println!("range {} - {}", minmax.0, minmax.1);
@@ -29,14 +32,33 @@ fn main() {
     let mut minpos = 0;
 
     for p in minmax.0..=minmax.1 {
-        let fuel = calcfuel(&data, p);
-        println!("pos {} fuel {}", p, fuel);
+        let fuelt = calcfuel(&data, p) as isize;
+        println!("pos {} fuel {}", p, fuelt);
 
-        if fuel < minfuel {
-            minfuel = fuel;
+        if fuelt < minfuel {
+            minfuel = fuelt;
             minpos = p;
         }
     }
 
     println!("minpos {} minfuel {}", minpos, minfuel)
+}
+
+fn calcexpfuel(distance: usize) -> usize {
+    let mut total = 0;
+
+    for i in 0..=distance {
+        total += i;
+    }
+
+    return total;
+}
+
+#[test]
+fn test() {
+    assert_eq!(calcexpfuel(1), 1);
+    assert_eq!(calcexpfuel(2), 3);
+    assert_eq!(calcexpfuel(3), 6);
+    assert_eq!(calcexpfuel(4), 10);
+    assert_eq!(calcexpfuel(5), 15);
 }
