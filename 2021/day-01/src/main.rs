b39be2e65df3a4c0071ffb59a44c8486f218574e
Rouan van der Ende <rouan@8bo.org>
2023-03-22 09:33:26 +0200

    improve 2021 day-01

--- 2021/day-01/src/main.rs
+++ 2021/day-01/src/main.rs
@@ -1,80 +1,60 @@
-use std::fs;
-
-fn day1_part1() {
-    let data = match std::fs::read_to_string("sample.txt") {
-        Ok(data) => data
-            .trim()
-            .split("\n")
-            .map(|x| {
-                println!("raw: {}", x);
-                let a = match x.parse::<usize>() {
-                    Ok(a) => a,
-                    Err(err) => panic!("Error: {}", err),
-                };
-                println!("parsed: {}", a);
-                return a;
-            })
-            .collect::<Vec<usize>>(),
-        Err(err) => panic!("Error: {}", err),
-    };
-
-    dbg!(&data);
-
-    // day 1
-
-    let mut previous = usize::MAX;
-    let mut increases = 0;
-
-    for i in data {
-        if i > previous {
-            println!("increase! {} > {}", i, previous);
-            increases += 1;
-        }
-        previous = i;
-        println!("{}", i);
-    }
-
-    println!("Increases: {}", increases);
+struct Submarine {
+    scanner_readings: Vec<usize>,
 }
 
-////////////////////////////////////////////////////////////////////
+impl Submarine {
+    fn load_data(filename: &str) -> Self {
+        let data = std::fs::read_to_string(filename).unwrap();
 
-fn day1_part2() {
-    let data = match std::fs::read_to_string("datainput.txt") {
-        Ok(data) => data
+        let scanner_readings = data
             .trim()
             .split("\n")
-            .map(|x| {
-                println!("raw: {}", x);
-                let a = match x.parse::<usize>() {
-                    Ok(a) => a,
-                    Err(err) => panic!("Error: {}", err),
-                };
-                println!("parsed: {}", a);
-                return a;
-            })
-            .collect::<Vec<usize>>(),
-        Err(err) => panic!("Error: {}", err),
-    };
+            .map(|x| x.trim().parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
 
-    println!("-----------------");
-    let mut increases = 0;
-    for i in 0..data.len() - 3 {
-        let a = data[i] + data[i + 1] + data[i + 2];
-        let b = data[i + 1] + data[i + 2] + data[i + 3];
-
-        if b > a {
-            increases += 1;
-        }
-
-        println!("total: {}", a);
-        println!("{}", i);
-        println!("{}", data[i])
+        Self { scanner_readings }
     }
 
-    println!("Increases: {}", increases);
+    fn calculate_depth_changes(&self) -> usize {
+        let mut previous = 0;
+        let mut depth_changes = 0;
+        self.scanner_readings
+            .iter()
+            .enumerate()
+            .for_each(|(count, reading)| {
+                if count > 0 && reading > &previous {
+                    depth_changes += 1;
+                }
+
+                previous = *reading;
+            });
+
+        return depth_changes;
+    }
+
+    fn calculate_depth_changes_window(&self) -> usize {
+        let mut increases = 0;
+        let data = &self.scanner_readings;
+        for i in 0..data.len() - 3 {
+            let a = data[i] + data[i + 1] + data[i + 2];
+            let b = data[i + 1] + data[i + 2] + data[i + 3];
+
+            if b > a {
+                increases += 1;
+            }
+        }
+
+        return increases;
+    }
 }
 
 fn main() {
-    day1_part2();
+    // let sub = Submarine::load_data("sample.txt");
+    let sub = Submarine::load_data("input.txt");
+
+    let part1 = sub.calculate_depth_changes();
+    println!("Depth changes: {}", part1);
+
+    let part2 = sub.calculate_depth_changes_window();
+    println!("Depth changes window: {}", part2);
 }
