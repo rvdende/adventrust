3f03896c1225f408c8d29f4498b2b49d2bb94109
Rouan van der Ende <rouan@8bo.org>
2023-04-10 09:37:41 +0200

    2022 day 10 part 2

--- src/advent2022/day10/mod.rs
+++ src/advent2022/day10/mod.rs
@@ -1,67 +1,182 @@
-#[derive(Debug)]
-struct Instr {
-    cycle: usize,
-    addx: i32,
-    instr: String,
+use std::{cmp::Ordering, str::Lines};
+
+trait Instruction {
+    fn do_work(&mut self, register_x: &mut i32) -> bool;
+    fn get_type(&self) -> String;
 }
-fn process(filename: &str) {
-    let instructions: Vec<Instr> = std::fs::read_to_string(filename)
-        .unwrap()
-        .lines()
-        .map(|l| {
-            if l.starts_with("addx") {
-                return Instr {
-                    cycle: 2,
-                    addx: l.split(" ").nth(1).unwrap().parse().unwrap(),
-                    instr: l.to_string(),
-                };
-            }
-            // noop
-            return Instr {
-                cycle: 1,
-                addx: 0,
-                instr: l.to_string(),
-            };
-        })
-        .collect();
 
-    println!("{:?}", instructions);
+struct NoopInstruction {
+    cycles_remaining: i32,
+}
 
-    let mut cycle = 0;
-    let mut done = false;
-    let mut cycle_next = 1;
-    let mut instruction_id = 0;
-
-    let mut x = 1;
-
-    while !done {
-        cycle += 1;
-        let instr = &instructions[instruction_id];
-        // cycle
-        println!(
-            "start x: {}, cycle {} id: {} - {} [{}]",
-            x, cycle, instruction_id, &instructions[instruction_id].instr, cycle_next
-        );
-
-        if cycle >= cycle_next {
-            x += instr.addx;
-
-            cycle_next = cycle + instr.cycle;
-            instruction_id = (instruction_id + 1) % instructions.len();
-        }
-
-        println!(
-            "  end x: {}, cycle {} id: {} - {} [{}]",
-            x, cycle, instruction_id, &instructions[instruction_id].instr, cycle_next
-        );
-
-        if cycle > 20 {
-            done = true;
+impl NoopInstruction {
+    fn new() -> NoopInstruction {
+        NoopInstruction {
+            cycles_remaining: 1,
         }
     }
 }
 
+impl Instruction for NoopInstruction {
+    fn do_work(&mut self, _: &mut i32) -> bool {
+        self.cycles_remaining -= 1;
+        if self.cycles_remaining <= 0 {
+            true
+        } else {
+            self.cycles_remaining -= 1;
+            false
+        }
+    }
+
+    fn get_type(&self) -> String {
+        "noop".to_string()
+    }
+}
+
+struct AddXInstruction {
+    cycles_remaining: i32,
+    x: i32,
+}
+
+impl AddXInstruction {
+    fn new(x: i32) -> AddXInstruction {
+        AddXInstruction {
+            cycles_remaining: 2,
+            x,
+        }
+    }
+}
+
+impl Instruction for AddXInstruction {
+    fn do_work(&mut self, register_x: &mut i32) -> bool {
+        self.cycles_remaining -= 1;
+        if self.cycles_remaining <= 0 {
+            *register_x += self.x;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn get_type(&self) -> String {
+        format!("addx {}", self.x)
+    }
+}
+
+fn load_instruction(lines: &mut Lines, current_instruction: &mut Box<dyn Instruction>) -> bool {
+    let next_line = lines.next();
+    if let Some(instruction) = next_line {
+        match &instruction[0..4] {
+            "noop" => {
+                *current_instruction = Box::new(NoopInstruction::new());
+            }
+            "addx" => {
+                *current_instruction = Box::new(AddXInstruction::new(
+                    instruction[5..].parse::<i32>().unwrap(),
+                ));
+            }
+            _ => panic!("Unknown instruction"),
+        }
+        return true;
+    }
+
+    return false;
+}
+
+fn process(filename: &str) -> (i32, String) {
+    let mut data = std::fs::read_to_string(filename)
+        .unwrap()
+        .trim()
+        .to_string();
+    let mut lines = data.lines();
+
+    let mut cycle = 0;
+    let mut register_x = 1;
+
+    let mut current_instruction: Box<dyn Instruction> = Box::new(NoopInstruction::new());
+    let mut end_of_program = false;
+    let mut sum_signal = 0;
+
+    let mut screen: Vec<bool> = Vec::new();
+
+    while !end_of_program {
+        if current_instruction.do_work(&mut register_x) {
+            if !load_instruction(&mut lines, &mut current_instruction) {
+                end_of_program = true;
+            }
+        }
+
+        cycle += 1;
+
+        if !end_of_program && (cycle - 20) % 40 == 0 {
+            let signal_strength = cycle * register_x;
+            println!(
+                "cycle: {}, register_x: {}, signal_strength: {}, instru: {}",
+                cycle,
+                register_x,
+                signal_strength,
+                current_instruction.get_type()
+            );
+
+            sum_signal += cycle * register_x
+        }
+
+        // par2 calculate sprite
+
+        let screenx = (cycle - 1) % 40;
+
+        println!("screenx: {}, reg_x: {}", screenx, &register_x);
+
+        if (screenx - register_x).abs() <= 1 {
+            screen.push(true);
+        } else {
+            screen.push(false);
+        }
+
+        // screen.push(true);
+    }
+
+    println!("sum_signal: {}", sum_signal);
+
+    let part1 = sum_signal;
+    let part2 = display_screen(screen);
+
+    (part1, part2)
+}
+
+fn display_screen(screen: Vec<bool>) -> String {
+    let mut lines: String = String::new();
+    let mut line = String::new();
+
+    screen.iter().enumerate().for_each(|(i, pixel)| {
+        if i % 40 == 0 {
+            lines = format!("{lines}\n{line}");
+            line = String::new();
+        }
+
+        if *pixel {
+            line.push('#');
+        } else {
+            line.push(' ');
+        }
+    });
+
+    println!("{}", lines);
+
+    lines.trim().to_string()
+}
+
 pub fn run() {
-    process("src/advent2022/day10/basic.txt");
+    // process("src/advent2022/day10/basic.txt");
     // process("src/advent2022/day10/sample.txt");
+
+    process("src/advent2022/day10/input.txt");
+}
+
+#[test]
+fn sample() {
+    let (part1, part2) = process("src/advent2022/day10/sample.txt");
+    assert_eq!(part1, 13140);
+    let part2_expected = "##..##..##..##..##..##..##..##..##..##..\n###...###...###...###...###...###...###.\n####....####....####....####....####....\n#####.....#####.....#####.....#####.....\n######......######......######......###.\n#######.......#######.......#######.....";
+    assert_eq!(part2, part2_expected.trim());
 }
