c85edff1f751078f5a1baa2726f37059bd640e6e
Rouan van der Ende <rouan@8bo.org>
2023-04-10 14:05:39 +0200

    day 11 wip

---
+++ src/advent2022/day11/mod.rs
@@ -0,0 +1,11 @@
+// https://www.youtube.com/watch?v=0RkTrYDyzmE
+
+// ref https://github.com/ChristopherBiscardi/advent-of-code/blob/528af99ac5a0d73dde585464a062fe9397b91b2d/2022/rust/day-11/src/lib.rs
+
+fn process(path: &str) {
+    let data = std::fs::read_to_string(path).unwrap();
+}
+
+pub fn run() {
+    process("src/advent2022/day11/sample.txt");
+}
--- src/advent2022/mod.rs
+++ src/advent2022/mod.rs
@@ -17,3 +17,5 @@ pub mod day08;
 pub mod day09;
 
 pub mod day10;
+
+pub mod day11;
