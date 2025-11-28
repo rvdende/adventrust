3aca22b8536aef4fefefa984ffe1f2ed613fcf3f
Rouan van der Ende <rouan@netron.co.za>
2025-11-28 17:54:50 +0200

    practice cleanup

--- src/advent2021/day01/mod.rs
+++ src/advent2021/day01/mod.rs
@@ -2,17 +2,36 @@ struct Submarine {
     scanner_readings: Vec<usize>,
 }
 
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum SubmarineErrors {
+    DataIOError(std::io::Error),
+    DataNumberParsingError(std::num::ParseIntError),
+}
+
+impl From<std::io::Error> for SubmarineErrors {
+    fn from(value: std::io::Error) -> Self {
+        SubmarineErrors::DataIOError(value)
+    }
+}
+
+impl From<std::num::ParseIntError> for SubmarineErrors {
+    fn from(value: std::num::ParseIntError) -> Self {
+        SubmarineErrors::DataNumberParsingError(value)
+    }
+}
+
 impl Submarine {
-    fn load_data(filename: &str) -> Self {
-        let data = std::fs::read_to_string(filename).unwrap();
+    fn load_data(filename: &str) -> Result<Self, SubmarineErrors> {
+        let data = std::fs::read_to_string(filename)?;
 
         let scanner_readings = data
             .trim()
             .split('\n')
-            .map(|x| x.trim().parse::<usize>().unwrap())
-            .collect::<Vec<usize>>();
+            .map(|x| x.trim().parse::<usize>())
+            .collect::<Result<Vec<usize>, _>>()?;
 
-        Self { scanner_readings }
+        Ok(Self { scanner_readings })
     }
 
     fn calculate_depth_changes(&self) -> usize {
@@ -48,8 +67,8 @@ impl Submarine {
     }
 }
 #[allow(dead_code)]
-fn main() {
-    let sub = Submarine::load_data("src/advent2021/day01/sample.txt");
+fn main() -> Result<(), SubmarineErrors> {
+    let sub = Submarine::load_data("src/advent2021/day01/sample.txt")?;
     //let sub = Submarine::load_data("day01/input.txt");
 
     let part1 = sub.calculate_depth_changes();
@@ -57,12 +76,14 @@ fn main() {
 
     let part2 = sub.calculate_depth_changes_window();
     println!("Depth changes window: {}", part2);
+
+    Ok(())
 }
 
 #[test]
-pub fn test() {
+pub fn test() -> Result<(), SubmarineErrors> {
     // sample
-    let sample_sub = Submarine::load_data("src/advent2021/day01/sample.txt");
+    let sample_sub = Submarine::load_data("src/advent2021/day01/sample.txt")?;
     let sample_part1 = sample_sub.calculate_depth_changes();
     let sample_part2 = sample_sub.calculate_depth_changes_window();
 
@@ -70,10 +91,12 @@ pub fn test() {
     assert_eq!(sample_part2, 5);
 
     // full
-    let full_sub = Submarine::load_data("src/advent2021/day01/input.txt");
+    let full_sub = Submarine::load_data("src/advent2021/day01/input.txt")?;
     let full_part1 = full_sub.calculate_depth_changes();
     let full_part2 = full_sub.calculate_depth_changes_window();
 
     assert_eq!(full_part1, 1482);
     assert_eq!(full_part2, 1518);
+
+    Ok(())
 }
