737529937a9538702e11e5e28a56738bf7db6688
Rouan van der Ende <rouan@netron.co.za>
2025-11-29 08:07:07 +0200

    2024 day1

---
+++ src/advent2023/day01/mod.rs
@@ -0,0 +1,105 @@
+use core::num;
+
+use crate::AdventError;
+
+struct WeatherMap {
+    lines: Vec<String>,
+}
+
+enum NumberWords {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+}
+
+impl NumberWords {
+    pub fn to_word(&self) -> &'static str {
+        match self {
+            NumberWords::Zero => "zero",
+            NumberWords::One => "one",
+            NumberWords::Two => "two",
+            NumberWords::Three => "three",
+            NumberWords::Four => "four",
+            NumberWords::Five => "five",
+            NumberWords::Six => "six",
+            NumberWords::Seven => "seven",
+            NumberWords::Eight => "eight",
+            NumberWords::Nine => "nine",
+        }
+    }
+
+    pub fn to_number(&self) -> usize {
+        match self {
+            NumberWords::Zero => 0,
+            NumberWords::One => 1,
+            NumberWords::Two => 2,
+            NumberWords::Three => 3,
+            NumberWords::Four => 4,
+            NumberWords::Five => 5,
+            NumberWords::Six => 6,
+            NumberWords::Seven => 7,
+            NumberWords::Eight => 8,
+            NumberWords::Nine => 9,
+        }
+    }
+}
+
+impl WeatherMap {
+    pub async fn from_file(filename: String) -> Result<Self, AdventError> {
+        let data = tokio::fs::read_to_string(filename).await?;
+
+        let parsed = data
+            .trim()
+            .split("\n")
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>();
+
+        Ok(Self {
+            lines: parsed.clone(),
+        })
+    }
+
+    pub fn calculate(&self) -> usize {
+        // read first character and last character
+        let numberlines = &self
+            .lines
+            .iter()
+            .map(|line| {
+                let parsed: Vec<usize> = line
+                    .chars()
+                    .map(|c| c.to_digit(10).unwrap_or(0) as usize)
+                    .filter(|x| *x > 0)
+                    .collect::<Vec<usize>>();
+
+                let first = parsed.first().unwrap();
+                let last = parsed.last().unwrap();
+                format!("{}{}", first, last).parse::<usize>().unwrap()
+            })
+            .collect::<Vec<usize>>();
+
+        let total = numberlines.iter().sum::<usize>();
+
+        total
+    }
+}
+
+#[tokio::test]
+pub async fn run() -> Result<(), AdventError> {
+    println!("asdasds");
+
+    let total = WeatherMap::from_file("src/advent2023/day01/p1_example.txt".to_string()).await?;
+    assert!(total.calculate() == 142);
+
+    let total = WeatherMap::from_file("src/advent2023/day01/input.txt".to_string()).await?;
+    assert!(total.calculate() == 54708);
+
+    println!("Running Advent of Code 2023 - Day 01");
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
 mod advent2021;
 mod advent2022;
 
+mod advent2023;
 mod advent2024;
 
-fn main() {
-    advent2024::advent202404::run();
+#[tokio::main]
+async fn main() -> Result<(), AdventError> {
+    // advent2023::day01::run();
+    Ok(())
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum AdventError {
+    DataIOError(std::io::Error),
+    DataNumberParsingError(std::num::ParseIntError),
+}
+
+impl From<std::io::Error> for AdventError {
+    fn from(value: std::io::Error) -> Self {
+        AdventError::DataIOError(value)
+    }
+}
+
+impl From<std::num::ParseIntError> for AdventError {
+    fn from(value: std::num::ParseIntError) -> Self {
+        AdventError::DataNumberParsingError(value)
+    }
 }
